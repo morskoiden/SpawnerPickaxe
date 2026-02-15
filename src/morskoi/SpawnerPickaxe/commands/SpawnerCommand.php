b0c7dc9bdb508be8139017d615899540f42e1ec0
morskoi den <deniscernysenko228@gmail.com>
2026-02-15 14:28:03 +0200

    Refactor SpawnerCommand for clarity and structure

--- src/morskoi/SpawnerPickaxe/commands/SpawnerCommand.php
+++ src/morskoi/SpawnerPickaxe/commands/SpawnerCommand.php
@@ -5,7 +5,6 @@ namespace morskoi\SpawnerPickaxe\commands;
 use pocketmine\command\{Command, CommandSender};
 use pocketmine\player\Player;
 use pocketmine\item\StringToItemParser;
-use pocketmine\block\VanillaBlocks;
 use morskoi\SpawnerPickaxe\SpawnerPickaxe;
 
 class SpawnerCommand extends Command
@@ -25,7 +24,7 @@ class SpawnerCommand extends Command
             $s->sendMessage("Only in game");
             return;
         }
-        $item = StringToItemParser::getInstance()->parse($this->plugin->getConfig()->get("material:", "GOLDEN_PICKAXE"));
+        $item = StringToItemParser::getInstance()->parse($this->plugin->getConfig()->get("material", "GOLDEN_PICKAXE"));
         $name = $this->plugin->getConfig()->get("name", "§e⛏️ Spawner Pickaxe ⛏️");
         $lore = $this->plugin->getConfig()->get("lore", "§7[ §6Breaks the §lspawner§r§6 and there is a §c50% chance §6it will drop.§r§7 ]");
         $item->setCustomName($name);
@@ -34,4 +33,5 @@ class SpawnerCommand extends Command
         $s->getInventory()->addItem($item);
         $s->sendMessage($this->plugin->getConfig()->get("message", "§fYou received a pickaxe that breaks spawners with a 50% chance."));
     }
+
 }
